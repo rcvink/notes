57412baf4d8269bf0beacc3825df6a87f6e9e93c
Ellie <elliewem@gmail.com>
2018-02-12 16:39:27 +0000

    Add note class with capability to store text

---
+++ spec/note-model-spec.js
@@ -0,0 +1,12 @@
+(function(exports) {
+  function testNoteTextIsStored() {
+    var note = new Note("Javascript is my favourite language");
+
+    if (note.text !== "Javascript is my favourite language") {
+      throw Error("Text is not 'Javascript is my favourite language'")
+    }
+    console.log("testNoteTextIsStored = passed!")
+  };
+
+  testNoteTextIsStored();
+})(this);
