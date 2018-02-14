a366bd5e3cbee67ac72b58be6f824a970963d91c
rcvink <rrvink@gmail.com>
2018-02-14 13:23:55 +0000

    Modify type assertion failure message

--- spec/assert.js
+++ spec/assert.js
@@ -1,14 +1,17 @@
 var assert = {
+
   isTrue: function(assertionToCheck, name) {
     if (!assertionToCheck) {
       throw Error(`Fail: ${name}. Expected true but received ${assertionToCheck}.`);
     };
     console.log(`Pass: ${name}.`);
   },
+
   isA: function(instance, type, name) {
     if (!(instance instanceof type)) {
       throw Error(`Fail ${name}. Expected ${type.name} but received ${instance.constructor.name}.`);
     };
     console.log(`Pass: ${name}.`);
   }
+
 };
