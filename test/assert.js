edfe87fdeea2cfd15156138fad464cacf9c8b47c
rcvink <rrvink@gmail.com>
2018-02-15 10:20:18 +0000

    Add assertions

--- test/assert.js
+++ test/assert.js
@@ -12,6 +12,27 @@ var assert = {
       throw Error(`Fail ${name}. Expected ${type.name} but received ${instance.constructor.name}.`);
     };
     console.log(`Pass: ${name}.`);
+  },
+
+  isEqual: function(instance1, instance2, name) {
+    if (instance1 !== instance2) {
+      throw Error(`Fail ${name}. Expected ${instance1} to equal ${instance2}.`);
+    };
+    console.log(`Pass: ${name}.`);
+  },
+
+  isEmpty: function(array, name) {
+    if (array.length !== 0) {
+      throw Error(`Fail ${name}. Expected ${array} to be empty but length was ${array.length}.`);
+    };
+    console.log(`Pass: ${name}.`);
+  },
+
+  contains: function(array, item, name) {
+    if (!array.includes(item)) {
+      throw Error(`Fail ${name}. Expected ${array} to include ${item} but contained ${array}`);
+    };
+    console.log(`Pass: ${name}.`);
   }
 
 };
