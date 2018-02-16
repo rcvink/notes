21033cac4a33a6e70ec38a44f94d3542a07b6c3e
rcvink <rrvink@gmail.com>
2018-02-16 02:38:07 +0000

    Extend test framework functionality

--- test/assert.js
+++ test/assert.js
@@ -4,35 +4,36 @@ var assert = {
     if (!assertionToCheck) {
       throw Error(`Fail: ${name}. Expected true but received ${assertionToCheck}.`);
     };
-    console.log(`Pass: ${name}.`);
   },
 
   isA: function(instance, type, name) {
     if (!(instance instanceof type)) {
       throw Error(`Fail ${name}. Expected ${type.name} but received ${instance.constructor.name}.`);
     };
-    console.log(`Pass: ${name}.`);
   },
 
   isEqual: function(instance1, instance2, name) {
     if (instance1 !== instance2) {
       throw Error(`Fail ${name}. Expected ${instance1} to equal ${instance2}.`);
     };
-    console.log(`Pass: ${name}.`);
   },
 
   isEmpty: function(array, name) {
     if (array.length !== 0) {
       throw Error(`Fail ${name}. Expected ${array} to be empty but length was ${array.length}.`);
     };
-    console.log(`Pass: ${name}.`);
   },
 
   contains: function(array, item, name) {
     if (!array.includes(item)) {
       throw Error(`Fail ${name}. Expected ${array} to include ${item} but contained ${array}`);
     };
-    console.log(`Pass: ${name}.`);
+  },
+
+  is0: function(number, name) {
+    if(number !== 0) {
+      throw Error(`Fail ${name}. Expected ${number} to equal 0.`);
+    };
   }
 
 };
